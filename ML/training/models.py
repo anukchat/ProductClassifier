c79ebc1782f453d33b6a6040e54c262eda42801b
Anukool Chaturvedi <chaturvedianukool@gmail.com>
2021-09-14 16:57:19 +0530

    Initial checked in files

---
+++ ML/training/models.py
@@ -0,0 +1,46 @@
+import pytorch_lightning as pl
+from efficientnet_pytorch import EfficientNet
+from training import config
+import torch.nn.functional as F
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.metrics.functional import accuracy
+import torch.nn as nn
+import torch
+
+class ProductModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.efficient_net = EfficientNet.from_pretrained(
+            'efficientnet-b5', num_classes=config.CLASSES)
+        in_features = self.efficient_net._fc.in_features
+        self.efficient_net._fc = nn.Linear(in_features, config.CLASSES)
+
+    def forward(self,x):
+        out = self.efficient_net(x)
+        return out
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            self.parameters(), lr=1e-4, weight_decay=0.0001)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, step_size=2, gamma=0.1)
+        return [optimizer], [scheduler]
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch["x"], batch["y"]
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = accuracy(y_hat, y)
+        self.log("train_acc", acc, on_step=False,
+                 on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_loss", loss, on_step=False,
+                 on_epoch=True, prog_bar=True, logger=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch["x"], batch["y"]
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = accuracy(y_hat, y)
+        self.log("val_acc", acc, prog_bar=True, logger=True),
+        self.log("val_loss", loss, prog_bar=True, logger=True)
---
+++ ML/training/train.py
@@ -0,0 +1,27 @@
+from _typeshed import SupportsLenAndGetItem
+from ML.training.data import ProductDataset,ProductDataModule
+from ML.training.models import ProductModel
+from ML.training import config
+
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+
+pl.seed_everything(42)
+
+# Callbacks
+model_checkpoint = ModelCheckpoint(monitor="val_loss",
+                                   verbose=True,
+                                   filename="{epoch}_{val_loss:.4f}")
+early_stopping = EarlyStopping('val_loss', patience=4)
+
+
+dm = ProductDataModule()
+cassava_model = ProductModel()
+trainer = pl.Trainer(gpus=-1, max_epochs=12,
+                     callbacks=[model_checkpoint, early_stopping])
+trainer.fit(cassava_model, dm)
+
+# manually you can save best checkpoints -
+# trainer.save_checkpoint("cassava_efficient_net.ckpt")
+torch.save(dm.save_dict(), config.MODEL_NAME)
